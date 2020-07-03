443493ce78693438b18f9b658f4eccd10e5d7453
Martin Stamm <martin.stamm@camunda.com>
2020-07-03 13:28:26 +0200

    My first action is ready

--- index.js
+++ index.js
@@ -3,33 +3,18 @@
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  // Your code here
-  
-  app.log('Yay, the app was loaded!')
-
-
-  app.on(['pull_request.opened', 'issue.opened'], async context => {
+  app.on(['pull_request.opened', 'issues.opened'], async context => {
     const issueContext = context.issue();
 
     let membership = context.github.orgs.checkMembership({org: 'camunda', username: context.payload.sender.login}).then(res => {
       if(res.status === 204) {
-        const issueComment = context.issue({ body: 'Hey there colleague, thanks for creating a PR!' });
-        return context.github.issues.createComment(issueComment)
+         // const issueComment = context.issue({ body: 'Hey there colleague, thanks for creating a PR!' });
+         // return context.github.issues.createComment(issueComment)
       }
     }).catch(err => {
-      const issueComment = context.issue({ body: 'Thanks for opening this pull request! We will look into you changes and will come back to you. This can take up to 7 days.' });
+      const issueComment = context.issue({ body: 'Thanks for opening this pull request! We will look into you changes and will come back to you.\nWe will look into it ASAP. You can have a look at our [Contribution Guidelines](https://github.com/camunda/camunda-bpm-platform/blob/master/CONTRIBUTING.md)' });
+      context.github.issues.addLabels(context.issue({ issue_number: issueContext.number, labels: ['needs triage'] }));
       return context.github.issues.createComment(issueComment)
     });
-
-    context.github.issues.addLabels(context.issue({ issue_number: issueContext.number, labels: ['needs review'] }));
-
   });
-
-
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
